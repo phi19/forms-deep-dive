a11d3c06c5d1a041b29f4e007cbb49ac9f9a69dd
João Sá <joaofilipesa2020@gmail.com>
2026-09-04 21:08:57 +0100

    refactor(reactive-login): render reactive form instead of template-driven-form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from './auth/login/login.component';
-import { TemplateDrivenLogin } from "./auth/template-driven-login/template-driven-login";
+import { ReactiveLogin } from './auth/reactive-login/reactive-login';
 
 @Component({
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
-  imports: [LoginComponent, TemplateDrivenLogin],
+  imports: [ReactiveLogin],
 })
 export class App {}
