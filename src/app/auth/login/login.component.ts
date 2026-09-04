5c223fa4876c726c664a9f0fc5344e5c0deb2b6f
João Sá <joaofilipesa2020@gmail.com>
2026-09-04 20:31:07 +0100

    feat(form): setup template-driven forms approach and test specific validations

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(form: NgForm) {
+    if (form.form.invalid) {
+      return;
+    }
+
+    const email = form.form.controls['email'];
+    const password = form.form.controls['password'];
+
+    console.log(email, password);
+  }
+
+  isFormInvalid(form: NgForm): boolean {
+    return form.submitted && form.form.invalid;
+  }
+}
