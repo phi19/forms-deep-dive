6a104ad48de20e77370e206c276d42fa07d1f86f
João Sá <joaofilipesa2020@gmail.com>
2026-09-06 15:08:42 +0100

    feat(signup): configure email field and add validators

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { uniqueEmailValidator } from '../../validators/unique-email-validator';
+import { AuthService } from '../auth-service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  private authService = inject(AuthService);
+
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      asyncValidators: [uniqueEmailValidator(this.authService)],
+    }),
+  });
+}
