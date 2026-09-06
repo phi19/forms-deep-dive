f83f6d5dd03b571f1ff6fa78abb462f26f79a75c
João Sá <joaofilipesa2020@gmail.com>
2026-09-06 15:03:43 +0100

    feat(signup): swtich to signup form and fill the autocomplete fields

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ReactiveLogin } from './auth/reactive-login/reactive-login';
+import { SignupComponent } from "./auth/signup/signup.component";
 
 @Component({
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
-  imports: [ReactiveLogin],
+  imports: [ReactiveLogin, SignupComponent],
 })
 export class App {}
